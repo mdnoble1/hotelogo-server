1f0c0e24e97ee097556baf7ee1401d4c61be1c0e
Md Noble <mdnoble.wd@gmail.com>
2023-11-05 23:57:57 +0600

    created basic server for hoteloGo

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middle wares
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('hoteloGo server is running');
+  });
+  
+  app.listen(port, () => {
+    console.log(`hoteloGo server running on ${port}`);
+  })
