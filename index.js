fe825d749e1e2d0ec3254a600ec195c495202beb
Md Noble <mdnoble.wd@gmail.com>
2023-11-08 22:20:32 +0600

    changed url for cors

--- index.js
+++ index.js
@@ -11,7 +11,11 @@ const port = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cookieParser())
 app.use(cors({
-    origin:'http://localhost:5173',
+    origin:[
+      // 'http://localhost:5173',
+      'https://hotelogo-client.web.app',
+      'https://hotelogo-client.firebaseapp.com'
+  ],
     credentials: true
 }))
 
